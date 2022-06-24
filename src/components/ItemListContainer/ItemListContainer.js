4c97cb396ec3667b528aaec2537c4ce81703e170
Carolina Nardini <caronardini@gmail.com>
2022-06-23 23:45:23 -0300

    dESAFIO 4

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,23 @@
+import ItemCount from '../ItemCount/ItemCount';
 import './ItemListContainer.css';
 
 function ItemListContainer() {
   return (
     <div>
       <body className="body">
-          <h2>Productos saludables y frescos</h2>
+        <h2>Productos saludables y frescos</h2>
        
       </body>
+      <section className='card'>
+        <div className='centrado'>
+          <p>Nombre del producto</p>
+          <p>Foto del producto</p>
+          <p>Precio del producto</p>
+          <ItemCount/>
+          <button className='addCart'>Agregar al carrito</button>
+        </div>
+      </section>
+
     </div>
   );
 }
