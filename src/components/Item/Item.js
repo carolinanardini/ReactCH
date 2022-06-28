601c512e467f41cbb942c89782f00330792e2399
Carolina Nardini <caronardini@gmail.com>
2022-06-27 21:34:27 -0300

    desafio 4

---
+++ src/components/Item/Item.js
@@ -0,0 +1,23 @@
+import ItemCount from '../ItemCount/ItemCount';
+import './Item.css';
+
+function Item(props) {
+  return (
+    <div>
+      <section className='card'>
+        <div className='centrado'>
+          <p>{props.product}</p>
+          <p>{props.image}</p>
+          <p>{props.price}</p>
+          
+          <ItemCount/>
+          <button className='button'>Agregar al carrito</button>
+          <button className='button'>Ver detalle</button>
+        </div>
+      </section>
+
+    </div>
+  );
+}
+
+export default Item;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,24 +1,17 @@
-import ItemCount from '../ItemCount/ItemCount';
+import ItemList from '../Body/ItemList';
 import './ItemListContainer.css';
+
 
 function ItemListContainer() {
   return (
-    <div>
-      <body className="body">
-        <h2>Productos saludables y frescos</h2>
-       
-      </body>
-      <section className='card'>
-        <div className='centrado'>
-          <p>Nombre del producto</p>
-          <p>Foto del producto</p>
-          <p>Precio del producto</p>
-          <ItemCount/>
-          <button className='addCart'>Agregar al carrito</button>
-        </div>
-      </section>
+    
+    <body className="">
 
-    </div>
+      <h2>Productos saludables y frescos</h2>
+    
+        <ItemList/>
+    </body>
+
   );
 }
 
